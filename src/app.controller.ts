09ef0ee0d8062d81e30080d3ed429ea4d460a7a6
Paolo Posso <pvictorsys@gmail.com>
2020-06-06 16:03:31 -0300

    create reject queue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern } from '@nestjs/microservices';
+import { EventPattern, ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @Inject('ORDER_SERVICE') private readonly client: ClientProxy) {}
 
   @EventPattern('order_created')
   async handleMessagePrinted(data: Record<string, unknown>) {
-    console.log(data.text);
+    console.log(data);
+
+    if (parseInt((data.id).toString()) % 2 > 0)
+      this.client.emit<any>('order_created_rejected', data);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      { name: 'ORDER_SERVICE', 
+        transport: Transport.REDIS, 
+        options: {
+          url: 'redis://localhost:6379',
+        }},
+     ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
